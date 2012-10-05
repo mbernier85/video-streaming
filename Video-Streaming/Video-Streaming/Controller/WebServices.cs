1acc0b18df6b9445183bee85f329739858370a51
Michael Bernier <infosonic@gmail.com>
2012-10-04 22:59:52 -0400

    added property to stream model, added a mock webservice

---
+++ Video-Streaming/Video-Streaming/Controller/WebServices.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Video_Streaming.DataModel;
+
+namespace Video_Streaming
+{
+    class WebServices
+    {
+        public IList<StreamModel> getStreamList()
+        {
+            List<StreamModel> streamModels = new List<StreamModel>();
+            StreamModel model = new StreamModel();
+            model.name = "test";
+            model.title = "title test";
+            streamModels.Add(model);
+
+            return streamModels;
+        }
+    }
+}
--- Video-Streaming/Video-Streaming/DataModel/StreamModel.cs
+++ Video-Streaming/Video-Streaming/DataModel/StreamModel.cs
@@ -1,17 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Video_Streaming.DataModel
 {
     class StreamModel
     {
-        
+        [JsonProperty("embed_count")]
         public string embedCount { get; set; }
+
+        [JsonProperty]
         public string name { get; set; }
+
+        [JsonProperty("stream_count")]
         public string streamCount { get; set; }
+
+        [JsonProperty]
+        public string category { get; set; }
+
+        [JsonProperty]
+        public string title { get; set; }
     }
 }
