e43e903409d8e7313ef1653891cae1a1493c963c
Michael Bernier <infosonic@gmail.com>
2012-10-04 22:26:06 -0400

    added data model for stream

---
+++ Video-Streaming/Video-Streaming/DataModel/StreamModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Video_Streaming.DataModel
+{
+    class StreamModel
+    {
+        
+        public string embedCount { get; set; }
+        public string name { get; set; }
+        public string streamCount { get; set; }
+    }
+}
