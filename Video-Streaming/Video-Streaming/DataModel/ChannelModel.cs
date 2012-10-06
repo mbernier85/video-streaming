387b132f789579ca6f3499afde6d262c9fe7e9d1
Michael Bernier <infosonic@gmail.com>
2012-10-06 14:42:37 -0400

    added ChannelModel, updated StreamModel

---
+++ Video-Streaming/Video-Streaming/DataModel/ChannelModel.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Video_Streaming.DataModel
+{
+    class ChannelModel
+    {
+     
+        [JsonProperty("subcategory_title")]
+        public string subcategoryTitle { get; set; }
+
+        [JsonProperty]
+        public string subcategory { get; set; }
+
+        [JsonProperty("screen_cap_url_small")]
+        public string screenCapUrlSmall { get; set; }
+
+        [JsonProperty("screen_cap_url_medium")]
+        public string screenCapUrlMedium { get; set; }
+
+        [JsonProperty("screen_cap_url_huge")]
+        public string screenCapUrlHuge { get; set; }
+
+        [JsonProperty]
+        public string status { get; set; }
+
+        [JsonProperty]
+        public string category { get; set; }
+
+        [JsonProperty]
+        public string title { get; set; }
+
+        [JsonProperty("embed_code")]
+        public string embedCode { get; set; }
+
+        [JsonProperty]
+        public bool producer { get; set; }
+
+        [JsonProperty("image_url_tiny")]
+        public string imageUrlTiny { get; set; }
+
+        [JsonProperty("image_url_huge")]
+        public string imageUrlHuge { get; set; }
+
+        [JsonProperty("image_url_large")]
+        public string imageUrlLarge { get; set; }
+
+        [JsonProperty("image_url_small")]
+        public string imageUrlSmall { get; set; }
+
+        [JsonProperty("image_url_medium")]
+        public string imageUrlMedium { get; set; }
+
+        [JsonProperty]
+        public string timezone { get; set; }
+
+        [JsonProperty]
+        public int id { get; set; }
+
+        [JsonProperty("views_count")]
+        public string viewsCount { get; set; }
+
+        [JsonProperty("embed_enable")]
+        public bool embedEnable { get; set; }
+
+        [JsonProperty]
+        public string login { get; set; }
+
+        [JsonProperty]
+        public string tags { get; set; }
+
+        [JsonProperty]
+        public string categoryTitle { get; set; }
+
+        [JsonProperty("channel_url")]
+        public string channelUrl { get; set; }
+
+        [JsonProperty]
+        public string language { get; set; }
+
+        [JsonProperty]
+        public bool mature { get; set; }
+
+    }
+}
--- Video-Streaming/Video-Streaming/DataModel/StreamModel.cs
+++ Video-Streaming/Video-Streaming/DataModel/StreamModel.cs
@@ -9,6 +9,10 @@ namespace Video_Streaming.DataModel
 {
     class StreamModel
     {
+
+        [JsonProperty]
+        public ChannelModel channel { get; set; }
+
         [JsonProperty("embed_count")]
         public string embedCount { get; set; }
 
@@ -23,5 +27,71 @@ namespace Video_Streaming.DataModel
 
         [JsonProperty]
         public string title { get; set; }
+
+        [JsonProperty("channel_count")]
+        public int channelCount { get; set; }
+
+        [JsonProperty]
+        public string format { get; set; }
+
+        [JsonProperty]
+        public bool featured { get; set; }
+
+        [JsonProperty("site_count")]
+        public string siteCount { get; set; }
+
+        [JsonProperty("abuse_reported")]
+        public bool abuseReported { get; set; }
+
+        [JsonProperty("video_height")]
+        public int videoHeight { get; set; }
+
+        [JsonProperty]
+        public string language { get; set; }
+
+        [JsonProperty("video_bitrate")]
+        public double videoBitrate { get; set; }
+
+        [JsonProperty]
+        public string id { get; set; }
+
+        [JsonProperty("meta_game")]
+        public string metaGame { get; set; }
+
+        [JsonProperty]
+        public string broadcaster { get; set; }
+
+        [JsonProperty("broadcaster_part")]
+        public int broadcastPart { get; set; }
+
+        [JsonProperty("audio_codec")]
+        public string audioCodec { get; set; }
+
+        [JsonProperty("up_time")]
+        public string upTime { get; set; }
+
+        [JsonProperty("video_width")]
+        public int videoWidth { get; set; }
+
+        [JsonProperty]
+        public string geo { get; set; }
+
+        [JsonProperty("channel_view_count")]
+        public int channelViewCount { get; set; }
+
+        [JsonProperty("channel_subscription")]
+        public bool channelSubscription { set; get; }
+
+        [JsonProperty("embed_enabled")]
+        public bool embedEnabled { get; set; }
+
+        [JsonProperty("stream_type")]
+        public string streamType { get; set; }
+
+        [JsonProperty("video_codec")]
+        public string videoCodec { get; set; }
+
+
+
     }
 }
