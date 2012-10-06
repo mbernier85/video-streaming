e4814d187877132f01babadfff67b76fd6831809
Marc-Andre <marc596>
2012-10-06 16:48:04 -0400

    Commit Justin service class

---
+++ Video-Streaming/Video-Streaming/Helper/Justin.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Video_Streaming.DataModel;
+
+namespace Video_Streaming
+{
+    class Justin
+    {
+        public class Stream {
+            public static IEnumerable<StreamModel> List() {
+                List<StreamModel> streamModels = new List<StreamModel>();
+                StreamModel model = new StreamModel();
+                model.name = "test";
+                model.title = "title test";
+                streamModels.Add(model);
+
+                return streamModels;            
+            }
+        }
+
+        public class User { }
+
+        public class Channel { }
+
+        public class Fan { }
+
+        public class Clip { }
+
+        public class Chat { }
+
+        public class Account { }
+
+        public class Category { }
+
+
+    }
+}
